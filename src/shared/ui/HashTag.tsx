e68d092dce32a6693a6a3fa59b1418d2e6336bfd
eddacris <jetallavache@outlook.com>
2026-02-01 02:20:01 +0700

    fix: adjust the distance between tags

--- src/shared/ui/HashTag.tsx
+++ src/shared/ui/HashTag.tsx
@@ -12,6 +12,7 @@ export const HashTag: React.FC<HashTagProps> = ({ tag, id, index }) => {
       key={id || index}
       style={{
         background: "#f0f0f0",
+        margin: "0 2px",
         padding: "2px 8px",
         borderRadius: "12px",
         fontSize: "12px",
