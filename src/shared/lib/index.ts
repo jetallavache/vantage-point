10cdbea3e9f303fe816c6d9b608b1e9bbeb55692
eddacris <jetallavache@outlook.com>
2026-02-26 17:31:50 +0700

    chore: fix linting issues and update exports
    - Fix unused imports and variables
    - Update shared module exports
    - Clean up code

--- src/shared/lib/index.ts
+++ src/shared/lib/index.ts
@@ -1,5 +1,4 @@
 export { formatPublishDate, formatDateTime } from "./date-utils";
 export { showApiError } from "./error-handler";
-export { createZodRules } from "./zod-rules";
 export { sanitizeText, hasSuspiciousContent } from "./sanitizer";
 export { ValidationErrorCodes, errorMsg } from "./validation-errors";
